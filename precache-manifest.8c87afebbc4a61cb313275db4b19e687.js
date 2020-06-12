d454d23840219e15338370d10b5288746c53e058
Saad Iqbal <s1iqbal@ryerson.ca>
2020-06-11 20:14:19 -0400

    Updates

---
+++ precache-manifest.8c87afebbc4a61cb313275db4b19e687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411c1c0df51df2351a71b34746d747ef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "487a0563c0066f7b84d8",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e416789baba60e007de",
+    "url": "/portfolio/static/js/2.a617a02f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a617a02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487a0563c0066f7b84d8",
+    "url": "/portfolio/static/js/main.597b3fda.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
